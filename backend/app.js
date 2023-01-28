4dc844fff3113d1aeb0d949618bf98f206e25966
Evgeshka1103 <mamasoni2709@yandex.ru>
2023-01-29 00:56:36 +0300

    Изменил адрес локал хост

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 const options = {
   origin: [
-    'http://localhost:3000',
+    'http:127.0.0.1:3000',
     'https://evgeshka.nomoredomainsclub.ru',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
